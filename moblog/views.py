80a9dc8795cebf65960116aa7d4c00a00420bac6
harold <harold96full@gmail.com>
2018-11-17 21:03:08 -0300

    html y css avanzando

--- moblog/views.py
+++ moblog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .models import Bici
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'mobike/inicio.html')
