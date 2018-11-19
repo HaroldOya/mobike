0379ae43e6c91554cfd4860895e847f2e58d468b
Harold Oyarzun <harold96full@gmail.com>
2018-11-18 21:53:43 -0300

    final

--- moblog/models.py
+++ moblog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django import forms
+from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Bici(models.Model):
@@ -17,3 +20,5 @@ class Bici(models.Model):
 
     def __str__(self):
         return self.codigo
+
+       
